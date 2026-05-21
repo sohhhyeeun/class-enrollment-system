fb089258155f29c4e987c5f887b39d0ae9d702df
sohhhyeeun <syeankm12076@gmail.com>
2026-05-21 22:19:45 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/example/classenrollmentsystem/ClassEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classenrollmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
